9093beaf7be4f2d5983a6b42988217ab4a6e3215
makoge boris <makogeboris@gmail.com>
2025-03-17 16:57:07 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <h1 className="mt-0 bg-[var(--color-moderate-blue)] pt-1.5 text-2xl text-[var(--color-white)]">
+        Interactive comments section
+      </h1>
+    </main>
+  );
+}
+
+export default App;
